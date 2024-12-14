35ea62f4e1db23c3df40a4a9ab61bdbf32309c8a
podlaskialgorytm <live.m851@gmail.com>
2024-12-14 17:02:28 +0100

    feat: add matrix pointer to eliminate gauss func

--- src/gauss.h
+++ src/gauss.h
@@ -7,6 +7,6 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b);
+int eliminate(MatrixPtr mat, MatrixPtr b);
 
 #endif
