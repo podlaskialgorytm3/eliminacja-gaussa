e1949923f66b66d5a3328aaba58a572a3d9530ff
podlaskialgorytm <live.m851@gmail.com>
2024-12-14 17:55:23 +0100

    feat: changing letters

--- src/gauss.c
+++ src/gauss.c
@@ -4,7 +4,7 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(MatrixPtr mat, MatrixPtr b)
+int eliminate(MatrixPtr A, MatrixPtr b)
 {
 	/**
 	 * Tutaj należy umieścić właściwą implemntację.
--- src/gauss.h
+++ src/gauss.h
@@ -7,6 +7,6 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(MatrixPtr mat, MatrixPtr b);
+int eliminate(MatrixPtr A, MatrixPtr b);
 
 #endif
