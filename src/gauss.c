8bceaeb12cca0a4b6fb42845cda69d8b78994114
Wodzislaw <01197878@pw.edu.pl>
2024-12-14 18:59:20 +0100

    Added gauss elimination

--- src/gauss.c
+++ src/gauss.c
@@ -1,4 +1,6 @@
 #include "gauss.h"
+#include <stdio.h>
+
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -6,9 +8,65 @@
  */
 int eliminate(MatrixPtr A, MatrixPtr b)
 {
-	/**
-	 * Tutaj należy umieścić właściwą implemntację.
-	 */
+	int n = A->r;
+    
+    for (int k = 0; k < n; k++)
+	{
+        int max_row = k;
+        for (int i = k + 1; i < n; i++)
+		{
+            if (A->data[i][k] > A->data[max_row][k])
+			{
+                max_row = i;
+            }
+        }
+
+        if (max_row != k)
+		{
+            for (int j = k; j < n; j++)
+			{
+                double temp = A->data[k][j];
+                A->data[k][j] = A->data[max_row][j];
+                A->data[max_row][j] = temp;
+            }
+            double temp = b->data[k][0];
+            b->data[k][0] = b->data[max_row][0];
+            b->data[max_row][0] = temp;
+        }
+
+        for (int i = k + 1; i < n; i++)
+		{
+            if (A->data[i][k] != 0)
+			{
+                double factor = A->data[i][k] / A->data[k][k];
+
+				for (int j = k; j < n; j++)
+				{
+                    A->data[i][j] -= factor * A->data[k][j];
+                }
+                b->data[i][0] -= factor * b->data[k][0];
+            }
+        }
+    }
+
+	for(int k = 0; k < n; k++)
+	{
+		if(A->data[k][k]==0)
+		{
+			return 1;
+		}
+		else if(A->data[k][k] != 1)
+		{
+			double temp=A->data[k][k];
+
+			for(int j = k; j < n; j++)
+			{
+				A->data[k][j] = A->data[k][j] / temp;
+			}
+
+			b->data[k][0] = b->data[k][0] / temp;
+		}
+	}
 
 	return 0;
 }
